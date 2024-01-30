300b0d1a251b0a0eb19688c054a09b266ca97593
martap22 <pagielskamarta@gmail.com>
2024-01-30 13:05:16 +0100

    added reading .json files and filtering by ROI

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,29 @@
 #funkcje na wstepne przetworzenie obrazu
-#segmentacja Maja
-#pca Nadia
 import cv2
 import numpy as np
+import json
+import os
 
-def preprocessing(video_path):
+data_folder = os.getcwd()
+nslt_json_path = os.path.join(data_folder, 'nslt_100.json')
+wlasl_json_path = os.path.join(data_folder, 'WLASL_v0.3.json')
 
+with open(nslt_json_path, 'r') as file:
+    nslt_data = json.load(file)
+
+with open(wlasl_json_path, 'r') as file:
+    wlasl_data = json.load(file)
+
+def preprocessing(video_path, bbox, frame_start, frame_end):
+    #trzeba bedzie zaktualizowac dokumentacje Maja
     """Przetwarza obraz z pliku wideo stosując redukcję szumów i poprawę kontrastu.
     :param video_path: Ścieżka do pliku wideo do przetworzenia.
     :type video_path: str
     """
 
     cap = cv2.VideoCapture(video_path)
+    current_frame = 0
+    processed_frames = []
 
     if not cap.isOpened():
         print("Nie udało się otworzyć pliku wideo. Sprawdź ścieżkę.")
@@ -19,26 +31,33 @@ def preprocessing(video_path):
 
     while True:
         ret, frame = cap.read()
-        
-        if ret:
-            
-            median_filtered = cv2.medianBlur(frame, 5)
-            gaussian_filtered = cv2.GaussianBlur(frame, (5, 5), 0)
-            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 
-            if len(frame.shape) == 3: 
-                lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-                l, a, b = cv2.split(lab)
-                l_clahe = clahe.apply(l)
-                lab_clahe = cv2.merge((l_clahe, a, b))
-                clahe_filtered = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2BGR)
-            else:
-                clahe_filtered = clahe.apply(frame)
-
-           yield clahe_filtered
-
-        else:
+        if not ret:
             break
+        current_frame += 1
+
+        if current_frame < frame_start or (frame_end != -1 and current_frame > frame_end):
+            continue
+
+        #tniemy do regionu zainteresowania ROI
+        x, y, w, h = bbox
+        roi = frame[y:y + h, x:x + w]
+
+        roi_filtered = cv2.medianBlur(roi, 5)
+        roi_filtered = cv2.GaussianBlur(roi_filtered, (5, 5), 0)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        if len(frame.shape) == 3:
+            lab = cv2.cvtColor(roi_filtered), cv2.COLOR_BGR2LAB)
+            l, a, b = cv2.split(lab)
+            l_clahe = clahe.apply(l)
+            lab_clahe = cv2.merge((l_clahe, a, b))
+            clahe_filtered = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2BGR)
+        else:
+            clahe_filtered = clahe.apply(frame)
+
+        yield clahe_filtered
+
+        processed_frames.append(roi_filtered)
 
     cap.release()
 
