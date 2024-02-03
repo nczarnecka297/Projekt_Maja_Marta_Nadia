9b59e1a247c9242e25daa3671ed499cf0a6cc670
martap22 <pagielskamarta@gmail.com>
2024-02-03 16:50:21 +0100

    divide processing into batches

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -2,30 +2,24 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from joblib import dump
+from joblib import dump, load
 import cv2
 import numpy as np
 import json
 import os
-from skimage.feature import hog
 from skimage import exposure
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.model_selection import train_test_split
 
-data_folder = os.getcwd()
-nslt_json_path = os.path.join(data_folder, 'nslt_100.json')
+data_folder = os.path.join(r'data')
 wlasl_json_path = os.path.join(data_folder, 'WLASL_v0.3.json')
-
-with open(nslt_json_path, 'r') as file:
-    nslt_data = json.load(file)
 
 with open(wlasl_json_path, 'r') as file:
     wlasl_data = json.load(file)
 
 def preprocessing(video_path, bbox, frame_start, frame_end):
     #trzeba bedzie zaktualizowac dokumentacje Maja
-    """Przetwarza obraz z pliku wideo stosując redukcję szumów i poprawę kontrastu.
-    :param video_path: Ścieżka do pliku wideo do przetworzenia.
-    :type video_path: str
-    """
 
     cap = cv2.VideoCapture(video_path)
     current_frame = 0
@@ -45,7 +39,6 @@ def preprocessing(video_path, bbox, frame_start, frame_end):
         if current_frame < frame_start or (frame_end != -1 and current_frame > frame_end):
             continue
 
-        #tniemy do regionu zainteresowania ROI
         x, y, w, h = bbox
         roi = frame[y:y + h, x:x + w]
 
@@ -77,21 +70,27 @@ def extract_features(processed_frames):
         else:
             features.append(descriptors.flatten())
 
-    return np.array(features, dtype=object)
+    mean_features_length = int(np.mean([len(f) for f in features]))
+    padded_features = []
 
-features = []
-labels = []
+    for feature in features:
+        if len(feature) < mean_features_length:
+            padded_feature = np.pad(feature, (0, mean_features_length - len(feature)), 'constant')
+        else:
+            padded_feature = feature[:mean_features_length]
+        padded_features.append(padded_feature)
 
-features_train = []
-labels_train = []
+    return np.array(padded_features, dtype=object)
 
-features_test = []
-labels_test = []
+batch_size = 10
+features_folder = 'processed_features'
 
-features_val = []
-labels_val = []
+if not os.path.exists(features_folder):
+    os.makedirs(features_folder)
 
-subset_info = {}
+batch_features = []
+batch_labels = []
+batch_index = 1
 
 for entry in wlasl_data:
     gloss = entry['gloss']
@@ -103,45 +102,53 @@ for entry in wlasl_data:
         video_path = os.path.join(data_folder, f"{video_id}.mp4")
 
         if os.path.exists(video_path):
+            print(f"Processing video {video_id} for gloss {gloss}...")
             processed_frames = preprocessing(video_path, bbox, frame_start, frame_end)
             video_features = extract_features(processed_frames)
             video_features_flattened = np.concatenate(video_features)
-            features.append(video_features_flattened)
-            labels.append(gloss)
-
-            subset = subset_info.get(video_id)
-            if subset:
-                if subset == 'train':
-                    features_train.append(video_features_flattened)
-                    labels_train.append(gloss)
-                elif subset == 'test':
-                    features_test.append(video_features_flattened)
-                    labels_test.append(gloss)
-                elif subset == 'val':
-                    features_val.append(video_features_flattened)
-                    labels_val.append(gloss)
-
+            batch_features.append(video_features_flattened)
+            batch_labels.append(gloss)
+            print("Done")
         else:
-            print(f"Video {video_id} for gloss {gloss} not found in the data folder.")
+            pass#print(f"Video {video_id} for gloss {gloss} not found in the data folder.")
 
-features = np.array(features)
-labels = np.array(labels)
+        if len(batch_features) >= batch_size:
+            features_filename = os.path.join(features_folder, f"{batch_index}_batch_features.npy")
+            print(f"Saving features to {features_filename}...")
+            np.save(features_filename, np.array(batch_features))
+            print(f"Saved {len(batch_features)} features.")
+            batch_features = []
+            batch_labels = []
+            batch_index += 1
 
-min_length = min(len(f) for f in features)
+if batch_features:
+    features_filename = os.path.join(features_folder, f"{batch_index}_batch_features.npy")
+    np.save(features_filename, np.array(batch_features))
 
-features_train = np.array([f[:min_length] if len(f) > min_length else np.pad(f, (0, min_length - len(f)), 'constant') for f in features_train])
-features_test = np.array([f[:min_length] if len(f) > min_length else np.pad(f, (0, min_length - len(f)), 'constant') for f in features_test])
-features_val = np.array([f[:min_length] if len(f) > min_length else np.pad(f, (0, min_length - len(f)), 'constant') for f in features_val])
+loaded_features = []
+for filename in os.listdir(features_folder):
+    if filename.endswith("_batch_features.npy"):
+        batch_features = np.load(os.path.join(features_folder, filename), allow_pickle=True)
+        loaded_features.extend(batch_features)
 
-labels_train = np.array(labels_train)
-labels_test = np.array(labels_test)
-labels_val = np.array(labels_val)
+X_train, X_test, y_train, y_test = train_test_split(loaded_features, labels, test_size=0.2, random_state=42)
 
-pipeline = make_pipeline(StandardScaler(), PCA(n_components=0.95), SVC(kernel='rbf', class_weight='balanced'))
+features_as_strings = [" ".join(map(str, feature)) for feature in X_train]
+vectorizer = CountVectorizer()
+features_transformed = vectorizer.fit_transform(features_as_strings)
 
-pipeline.fit(features_train, labels_train)
+explained_variance_ratio = TruncatedSVD(n_components=features_transformed.shape[1]).fit(features_transformed).explained_variance_ratio_
+n_components = np.argmax(np.cumsum(explained_variance_ratio) >= 0.95)
+
+pipeline = make_pipeline(StandardScaler(with_mean=False), PCA(n_components=n_components), SVC(kernel='rbf', class_weight='balanced'))
+
+print("Training model..")
+pipeline.fit(features_transformed, y_train)
 
 dump(pipeline, 'sign_language_classifier.joblib')
 
-print(f"Training Accuracy: {pipeline.score(features_train, labels_train)}")
-print(f"Testing Accuracy: {pipeline.score(features_test, labels_test)}")
+X_test_as_strings = [" ".join(map(str, feature)) for feature in X_test]
+X_test_transformed = vectorizer.transform(X_test_as_strings)
+test_accuracy = pipeline.score(X_test_transformed, y_test)
+
+print(f"Accuracy on test data: {test_accuracy}")
